54fb4481116d4f5a00ed66087c367e7aa459b74d
atinm <atinm.dev@gmail.com>
2011-05-17 17:49:18 -0400

    Unhelpful's fix to match build
    Change-Id: Ic7973531e7d6710caad0538703843fc30f0ae198

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,7 +1,9 @@
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): device/samsung/aries-common/mkshbootimg.py \
-		$(TARGET_RECOVERY_COMPRESSED_INITRAMFS) \
-		$(BUILT_COMPRESSED_INITRAMFS_TARGET) \
-		$(TARGET_PREBUILT_KERNEL)
-	$(call pretty,"Boot image: $@")
-	$(hide) ./device/samsung/aries-common/mkshbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(BUILT_COMPRESSED_INITRAMFS_TARGET) $(TARGET_RECOVERY_COMPRESSED_INITRAMFS)
+$(INSTALLED_BOOTIMAGE_TARGET): device/samsung/common/aries/mkshbootimg.py \
+$(recovery_ramdisk) \
+$(INSTALLED_RAMDISK_TARGET) \
+$(TARGET_PREBUILT_KERNEL)
+$(call pretty,"Boot image: $@")
+$(hide) ./device/samsung/common/aries/mkshbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+
+INSTALLED_RECOVERYIMAGE_TARGET :=
