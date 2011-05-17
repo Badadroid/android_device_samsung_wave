bda6cbff1d31bfbe501fbb8604792324167fcc7c
Andrew Mahone <andrew.mahone@gmail.com>
2011-05-17 17:59:51 -0400

    Fix for upstream version of BOARD_CUSTOM_BOOTIMG_MK.
    Change-Id: I11fc6896c4faec5c19cda372f850ebdfe22c1ded

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,9 +1,9 @@
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): device/samsung/common/aries/mkshbootimg.py \
-$(recovery_ramdisk) \
-$(INSTALLED_RAMDISK_TARGET) \
-$(TARGET_PREBUILT_KERNEL)
-$(call pretty,"Boot image: $@")
-$(hide) ./device/samsung/common/aries/mkshbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+$(INSTALLED_BOOTIMAGE_TARGET): device/samsung/aries-common/mkshbootimg.py \
+		$(TARGET_PREBUILT_KERNEL)
+		$(recovery_ramdisk) \
+		$(INSTALLED_RAMDISK_TARGET) \
+	$(call pretty,"Boot image: $@")
+	$(hide) ./device/samsung/common/aries/mkshbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
 
-INSTALLED_RECOVERYIMAGE_TARGET :=
+INSTALLED_RECOVERYIMAGE_TARGET := 
