4f388a184d736fa6d9e9b4fe9be2523aefe2861d
Koushik Dutta <koushd@gmail.com>
2011-05-17 22:42:34 -0700

    boot image needs more dependencies to be properly flashable by the updater-script and the bml to mtd converstion process.
    Change-Id: Id551ac2dab9a1fb61dd08bf9d41d2b0734abff83

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,5 +1,5 @@
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET)
+$(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image
 	$(call pretty,"Boot image: $@")
 	$(hide) ./device/samsung/aries-common/mkshbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
 
