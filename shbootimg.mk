5633c07f601962688e0d4584803495f58ba16225
Koushik Dutta <koushd@gmail.com>
2011-05-17 17:50:41 -0700

    fix paths for aries-common. make the recovery target copy the boot image.
    Change-Id: I1dc243392374026e1bc4c8b3b15b4af20a574cc5

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,9 +1,8 @@
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): device/samsung/aries-common/mkshbootimg.py \
-		$(TARGET_PREBUILT_KERNEL)
-		$(recovery_ramdisk) \
-		$(INSTALLED_RAMDISK_TARGET) \
+$(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET)
 	$(call pretty,"Boot image: $@")
-	$(hide) ./device/samsung/common/aries/mkshbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+	$(hide) ./device/samsung/aries-common/mkshbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
 
-INSTALLED_RECOVERYIMAGE_TARGET := 
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET)
+	$(ACP) $(INSTALLED_BOOTIMAGE_TARGET) $@
+
