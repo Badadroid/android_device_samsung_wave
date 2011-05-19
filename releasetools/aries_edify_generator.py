df3cea31446851c6872ef744a2fc89898dd0380e
Koushik Dutta <koushd@gmail.com>
2011-05-19 14:35:14 -0700

    Obvious problem is obvious.
    Change-Id: Ib3ebf51ddb90cfee0e9ad74b2d6416d126ae4f3c

--- releasetools/aries_edify_generator.py
+++ releasetools/aries_edify_generator.py
@@ -34,6 +34,9 @@ class EdifyGenerator(edify_generator.EdifyGenerator):
             ('package_extract_file("updater.sh", "/tmp/updater.sh");\n'
              'set_perm(0, 0, 0777, "/tmp/updater.sh");'))
       self.script.append(
+           ('package_extract_file("make_ext4fs", "/tmp/make_ext4fs");\n'
+            'set_perm(0, 0, 0777, "/tmp/make_ext4fs");'))
+      self.script.append(
             ('package_extract_file("busybox", "/tmp/busybox");\n'
              'set_perm(0, 0, 0777, "/tmp/busybox");'))
       self.script.append(
