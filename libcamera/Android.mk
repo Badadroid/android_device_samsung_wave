bb5c28f6349e0b1f53fc27a6c3b66e57d5767edd
Pawit Pornkitprasan <p.pawit@gmail.com>
2011-08-08 14:33:55 +0700

    Replaced libcamera.so with a wrapper
    Features:
        - Continuous AF
        - ISO
        - Metering
        - Scene-modes
        - Focus-modes
        - Touch to focus (dependent on Iafc575a5)
        - Additional FFC Preview sizes (was hacked in on the old libcamera.so)
        - Disabled Samsung hacks in Camera.apk because it's no longer needed
    
    Note:
        - I believe that the same libcamera.so (now renamed libsamsungcamera.so)
          can be used for all variants now that the wrapper returns the
          correct number of comera (correct me if I'm wrong)
        - The vendor_samsung will need to be updated with libcamera.so removed
          for all models and http://www.mediafire.com/?3h7p4m0932l1dbv
          added as a proprietary file (renamed as libsamsungcamera.so).
          except for fascinatemtd, mesmerizemtd and showcasemtd and they
          require their libcamera.so with flash support. (Just rename
          the old one to libsamsungcamera.so)
        - This new libcamera.so requires a new prebuilt kernel (with
          pull request #41, which is already merged) for FFC to work.
          (only on models with FFC)
    
    Change-Id: I1f179068bf9d5295a48d118be191cf65c834fe7b

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -1,51 +1,19 @@
-ifeq ($(Y_U_NO_HAVE_CAMERA),true)
-
-# When zero we link against libqcamera; when 1, we dlopen libqcamera.
-ifeq ($(BOARD_CAMERA_LIBRARIES),libcamera)
-
-DLOPEN_LIBSECCAMERA:=1
-
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS:=-fno-short-enums
-LOCAL_CFLAGS+=-DDLOPEN_LIBSECCAMERA=$(DLOPEN_LIBSECCAMERA)
+LOCAL_SRC_FILES:= LibCameraWrapper.cpp
 
-ifdef BOARD_CAMERA_DEVICE
-    LOCAL_CFLAGS += -DCAMERA_DEV_NAME=\"$(BOARD_CAMERA_DEVICE)\"
-endif
+LOCAL_SHARED_LIBRARIES:= libdl libutils libcutils libcamera_client
+
+LOCAL_MODULE := libcamera
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PRELINK_MODULE := false
 
 ifdef BOARD_SECOND_CAMERA_DEVICE
-    LOCAL_CFLAGS += -DCAMERA_DEV_NAME2=\"$(BOARD_SECOND_CAMERA_DEVICE)\"
     LOCAL_CFLAGS += -DFFC_PRESENT
 endif
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libs3cjpeg
-
-
-LOCAL_SRC_FILES:= \
-	SecCamera.cpp \
-	SecCameraHWInterface.cpp
-
-
-LOCAL_SHARED_LIBRARIES:= libutils libui liblog libbinder libcutils
-LOCAL_SHARED_LIBRARIES+= libs3cjpeg.aries
-LOCAL_SHARED_LIBRARIES+= libcamera_client
-
-ifeq ($(BOARD_USES_OVERLAY),true)
-LOCAL_CFLAGS += -DBOARD_USES_OVERLAY
-endif
-
-ifeq ($(DLOPEN_LIBSECCAMERA),1)
-LOCAL_SHARED_LIBRARIES+= libdl
-endif
-
-LOCAL_MODULE:= libcamera
-
-LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
 
-endif
-endif
