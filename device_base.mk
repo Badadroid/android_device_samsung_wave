8a915b2362295db379071e5fa56a87d1df39ea83
Pawit Pornkitprasan <p.pawit@gmail.com>
2011-12-02 11:56:15 +0700

    Fixed compiling on userdebug

--- device_base.mk
+++ device_base.mk
@@ -70,7 +70,8 @@ PRODUCT_PACKAGES := \
 # Filesystem management tools
 PRODUCT_PACKAGES += \
 	make_ext4fs \
-	setup_fs
+	setup_fs \
+	bml_over_mtd
 
 # These are the OpenMAX IL configuration files
 PRODUCT_COPY_FILES += \
