4b9e2a9d20633fe41c13e7d40dc4cfb6b9b653be
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-01-26 20:58:48 +0700

    galaxysmtd: Add cm.mk

---
+++ cm.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, device/samsung/galaxysmtd/full_galaxysmtd.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := GalaxyS
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := galaxysmtd
+PRODUCT_NAME := cm_galaxysmtd
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-I9000
+
+#Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9000 TARGET_DEVICE=GT-I9000 BUILD_ID=GINGERBREAD BUILD_FINGERPRINT=samsung/GT-I9000/GT-I9000:2.3.5/GINGERBREAD/XXJVT:user/release-keys PRIVATE_BUILD_DESC="GT-I9000-user 2.3.5 GINGERBREAD XXJVT release-keys"
