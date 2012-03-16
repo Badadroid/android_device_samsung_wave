7714af5a22b42095785121c17dd4b3c8bdd30ca2
Daniel Bateman <jetison.24@gmail.com>
2012-03-15 22:17:30 -0500

    aries-common: allow for GPS init modularity

--- device_base.mk
+++ device_base.mk
@@ -50,6 +50,7 @@ PRODUCT_COPY_FILES := \
 # Init files
 PRODUCT_COPY_FILES += \
 	device/samsung/aries-common/init.aries.rc:root/init.aries.rc \
+	device/samsung/aries-common/init.aries.gps.rc:root/init.aries.gps.rc \
 	device/samsung/aries-common/init.aries.usb.rc:root/init.aries.usb.rc \
 	device/samsung/aries-common/init.aries.usb.rc:recovery/root/usb.rc \
 	device/samsung/aries-common/lpm.rc:root/lpm.rc \
