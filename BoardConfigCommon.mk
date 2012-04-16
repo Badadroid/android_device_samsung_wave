c9d2bf05f0c99d059485d136ac6a97a7d6abdc66
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-04-16 13:08:21 +0700

    aries-common: Support integrated kernel building
    - shbookimg.mk needs to be updated to use the built kernel
    - Added kernel source path
    
    Change-Id: Ic34bf51f22d091056940529df3313a3159b37857

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,6 +38,9 @@ TARGET_RECOVERY_INITRC := device/samsung/aries-common/recovery.rc
 BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
 BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
 
+# Kernel Source
+TARGET_KERNEL_SOURCE := kernel/samsung/aries
+
 # Releasetools
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/aries-common/releasetools/aries_ota_from_target_files
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/aries-common/releasetools/aries_img_from_target_files
--- shbootimg.mk
+++ shbootimg.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image $(PRODUCT_OUT)/modem.bin
+$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image $(PRODUCT_OUT)/modem.bin
 	$(call pretty,"Boot image: $@")
-	$(hide) ./device/samsung/aries-common/mkshbootimg.py $@ $(TARGET_PREBUILT_KERNEL) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+	$(hide) ./device/samsung/aries-common/mkshbootimg.py $@ $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET)
 	$(ACP) $(INSTALLED_BOOTIMAGE_TARGET) $@
