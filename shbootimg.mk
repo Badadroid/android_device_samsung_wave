9165c9f6690ab225eca5283f4467ab05ffbda794
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-07-21 10:09:55 +0700

    aries-common: built ramdisk into kernel
    Do not use the hackish append-ramdisk-to-kernel now that we can
    build the ramdisk right into the kernel with in-line kernel
    building.
    
    Change-Id: Id6cd448c7bede78d60f1c57c10447c9294a1e8d1

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,9 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
+# Add ramdisk dependencies to kernel
+TARGET_KERNEL_BINARIES: $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image $(PRODUCT_OUT)/modem.bin
+
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image $(PRODUCT_OUT)/modem.bin
+$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Boot image: $@")
-	$(hide) ./device/samsung/aries-common/mkshbootimg.py $@ $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+	$(hide) $(ACP) $(INSTALLED_KERNEL_TARGET) $@
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET)
 	$(ACP) $(INSTALLED_BOOTIMAGE_TARGET) $@
