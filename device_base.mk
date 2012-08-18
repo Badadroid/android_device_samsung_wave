9ed80aa8e2998f0b073a4948991abbc8609ec428
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-08-18 16:28:55 +0700

    aries-common: Use exynos3 common
    Change-Id: I337ba107f248992d7e08be9f45efeb78529f8ba8

--- device_base.mk
+++ device_base.mk
@@ -80,23 +80,23 @@ PRODUCT_PACKAGES += \
 
 # These are the OpenMAX IL configuration files
 PRODUCT_COPY_FILES += \
-	device/samsung/aries-common/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
+	hardware/samsung/exynos3/s5pc110/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
 	device/samsung/aries-common/media_profiles.xml:system/etc/media_profiles.xml \
 	device/samsung/aries-common/media_codecs.xml:system/etc/media_codecs.xml
 
 # These are the OpenMAX IL modules
 PRODUCT_PACKAGES += \
-	libSEC_OMX_Core.aries \
-	libOMX.SEC.AVC.Decoder.aries \
-	libOMX.SEC.M4V.Decoder.aries \
-	libOMX.SEC.M4V.Encoder.aries \
-	libOMX.SEC.AVC.Encoder.aries
+	libSEC_OMX_Core \
+	libOMX.SEC.AVC.Decoder \
+	libOMX.SEC.M4V.Decoder \
+	libOMX.SEC.M4V.Encoder \
+	libOMX.SEC.AVC.Encoder
 
 # Misc other modules
 PRODUCT_PACKAGES += \
 	lights.aries \
 	sensors.aries \
-	hwcomposer.aries \
+	hwcomposer.s5pc110 \
 	camera.aries \
 	audio.primary.aries \
 	audio_policy.aries \
