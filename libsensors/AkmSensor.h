11295cb7adb6d807bb8c48e1ce712681b6333536
Dominik Marszk <dmarszk@gmail.com>
2012-12-07 06:42:39 +0000

    Commented out unused interfaces. (Does it help in anything?)

--- libsensors/AkmSensor.h
+++ libsensors/AkmSensor.h
@@ -37,9 +37,9 @@ public:
     virtual ~AkmSensor();
 
     enum {
-        Accelerometer   = 0,
-        MagneticField   = 1,
-        Orientation     = 2,
+     //   Accelerometer   = 0,
+        MagneticField   = 0,
+     //   Orientation     = 2,
         numSensors
     };
 
