828fdae5dfc222dfbe0b4f46e360da3b6bfed593
Dominik Marszk <dmarszk@gmail.com>
2012-12-08 13:12:21 +0000

    Added setmodel.sh script content. model setprop might need fix (is it possible to do it from shell?)

--- prebuilt/setmodel.sh
+++ prebuilt/setmodel.sh
@@ -1,2 +1,8 @@
 #!/system/bin/sh
 
+if cat /proc/cpuinfo | grep wave2 ; then
+	ln /system/vendor/firmware/nvram_net_s8530.txt /system/vendor/firmware/nvram_net.txt 
+	setprop ro.product.model GT-S8530
+else
+	ln /system/vendor/firmware/nvram_net_s8500.txt /system/vendor/firmware/nvram_net.txt 
+fi
