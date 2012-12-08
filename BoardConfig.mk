873e5c952617ad52ce7ae9ab75aa19b5d099617d
Dominik Marszk <dmarszk@gmail.com>
2012-12-08 12:55:54 +0000

    Cleanup due to merging wave and wave2. Prepared setmodel.sh script for wave2 setup.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,118 @@ BOARD_HAVE_FM_RADIO := true
 BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 BOARD_FM_DEVICE := si4709
 
-TARGET_OTA_ASSERT_DEVICE := wave,GT-S8500,S8500
+BOARD_USES_GENERIC_AUDIO := false
 
-# Import the wave BoardConfigCommon.mk
-include device/samsung/wave/BoardConfigCommon.mk
+BOARD_USES_LIBSECRIL_STUB := true
+
+BOARD_NO_PAGE_FLIPPING := false
+BOARD_NO_32BPP := false
+
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+TARGET_PROVIDES_INIT := true
+TARGET_BOARD_PLATFORM := s5pc110
+TARGET_BOOTLOADER_BOARD_NAME := wave
+TARGET_RECOVERY_INITRC := device/samsung/wave/recovery.rc
+
+BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
+BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
+
+# Kernel Source
+TARGET_KERNEL_SOURCE := kernel/samsung/wave
+
+# Releasetools
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/wave/releasetools/wave_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/wave/releasetools/wave_img_from_target_files
+
+
+# Camera
+USE_CAMERA_STUB := false
+ifeq ($(USE_CAMERA_STUB),false)
+BOARD_CAMERA_LIBRARIES := libcamera
+endif
+BOARD_CAMERA_HAVE_FLASH := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Video Devices
+BOARD_V4L2_DEVICE := /dev/video1
+BOARD_CAMERA_DEVICE := /dev/video0
+
+TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 800
+
+BOARD_NAND_PAGE_SIZE := 4096
+BOARD_NAND_SPARE_SIZE := 128
+BOARD_KERNEL_BASE := 0x32000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_CMDLINE := console=ttyFIQ0,115200 init=/init no_console_suspend
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 7864320
+#350MB
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 393216000
+#400MB
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 419430400
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Connectivity - Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WLAN_DEVICE_REV       := bcm4329
+WIFI_DRIVER_MODULE_NAME     := "bcmdhd"
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+
+# adb has root
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+
+# Vold
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
+
+# Recovery
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/wave/recovery/graphics.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/wave/recovery/recovery_keys.c
+BOARD_USES_BML_OVER_MTD := true
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/wave/shbootimg.mk
+
+# Boot Animation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+# SkTextBox for libtvout
+BOARD_USES_SKTEXTBOX := true
+
+# Hardware rendering
+USE_OPENGL_RENDERER := true
+
+# TARGET_DISABLE_TRIPLE_BUFFERING can be used to disable triple buffering
+# on per target basis. On crespo it is possible to do so in theory
+# to save memory, however, there are currently some limitations in the
+# OpenGL ES driver that in conjunction with disable triple-buffering
+# would hurt performance significantly (see b/6016711)
+TARGET_DISABLE_TRIPLE_BUFFERING := false
+
+BOARD_ALLOW_EGL_HIBERNATION := true
+
+# Include wave specific stuff
+-include device/samsung/wave/Android.mk
+
--- device.mk
+++ device.mk
@@ -38,12 +38,169 @@
 DEVICE_PACKAGE_OVERLAYS += \
 	device/samsung/wave/overlay
 
-PRODUCT_COPY_FILES := \
-	device/samsung/wave/asound.conf:system/etc/asound.conf
-
 # We have FFC
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
-# Inherit Wave common device configuration.
-$(call inherit-product, device/samsung/wave/device_base.mk)
+DEVICE_PACKAGE_OVERLAYS := device/samsung/wave/overlay
+
+TARGET_RECOVERY_FSTAB := device/samsung/wave/recovery.fstab
+
+# These are the hardware-specific configuration files
+PRODUCT_COPY_FILES := \
+	device/samsung/wave/prebuilt/asound.conf:system/etc/asound.conf \
+	device/samsung/wave/prebuilt/vold.fstab:system/etc/vold.fstab \
+	device/samsung/wave/prebuilt/egl.cfg:system/lib/egl/egl.cfg \
+	device/samsung/wave/prebuilt/main.conf:system/etc/bluetooth/main.conf
+
+# Init files
+PRODUCT_COPY_FILES += \
+	device/samsung/wave/init.wave.rc:root/init.wave.rc \
+	device/samsung/wave/init.wave.rc:root/init.wave2.rc \
+	device/samsung/wave/init.wave.usb.rc:root/init.wave.usb.rc \
+	device/samsung/wave/init.wave.usb.rc:recovery/root/usb.rc \
+	device/samsung/wave/ueventd.wave.rc:root/ueventd.wave.rc \
+	device/samsung/wave/ueventd.wave.rc:root/ueventd.wave2.rc
+
+# Keylayout and Keychars
+PRODUCT_COPY_FILES += \
+	device/samsung/wave/prebuilt/usr/idc/mxt224_ts_input.idc:system/usr/idc/mxt224_ts_input.idc \
+	device/samsung/wave/prebuilt/usr/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+	device/samsung/wave/prebuilt/usr/keylayout/s5pv210-keypad.kl:system/usr/keylayout/s5pv210-keypad.kl \
+	device/samsung/wave/prebuilt/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+	device/samsung/wave/prebuilt/usr/keychars/s5pv210-keypad.kcm:system/usr/keychars/s5pv210-keypad.kcm \
+	device/samsung/wave/prebuilt/usr/keychars/sec_jack.kcm:system/usr/keychars/sec_jack.kcm \
+	device/samsung/wave/prebuilt/usr/keychars/gpio-keys.kcm:system/usr/keychars/gpio-keys.kcm
+
+# SHP Modem interfaces
+PRODUCT_PACKAGES := \
+	libsamsung-ipc \
+	ipc-modemctrl \
+	libsamsung-ril
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	make_ext4fs \
+	setup_fs \
+	bml_over_mtd
+
+# Usb accessory
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory
+
+# These are the OpenMAX IL configuration files
+PRODUCT_COPY_FILES += \
+	hardware/samsung/exynos3/s5pc110/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
+	device/samsung/wave/media_profiles.xml:system/etc/media_profiles.xml \
+	device/samsung/wave/media_codecs.xml:system/etc/media_codecs.xml
+
+# These are the OpenMAX IL modules
+PRODUCT_PACKAGES += \
+	libSEC_OMX_Core \
+	libOMX.SEC.AVC.Decoder \
+	libOMX.SEC.M4V.Decoder \
+	libOMX.SEC.M4V.Encoder \
+	libOMX.SEC.AVC.Encoder
+
+# Misc other modules
+PRODUCT_PACKAGES += \
+	lights.wave \
+	sensors.wave \
+	power.s5pc110 \
+	hwcomposer.s5pc110 \
+	camera.wave \
+	audio.primary.wave \
+	audio_policy.wave \
+	audio.a2dp.default \
+	audio.usb.default \
+	libs3cjpeg
+
+PRODUCT_COPY_FILES += \
+	device/samsung/wave/libaudio/audio_policy.conf:system/etc/audio_policy.conf
+
+# Libs
+PRODUCT_PACKAGES += \
+	libstagefrighthw
+
+# Bluetooth MAC Address
+PRODUCT_PACKAGES += \
+	bdaddr_read
+
+# Device-specific packages
+PRODUCT_PACKAGES += \
+	SamsungServiceMode \
+	AriesParts \
+	tvouthack \
+	Torch
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# The OpenGL ES API level that is natively supported by this device.
+# This is a 16.16 fixed point number
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.opengles.version=131072
+
+# These are the hardware-specific settings that are stored in system properties.
+# Note that the only such settings should be the ones that are too low-level to
+# be reachable from resources or other mechanisms.
+PRODUCT_PROPERTY_OVERRIDES += \
+       wifi.interface=wlan0 \
+       ro.telephony.ril_class=SamsungRIL \
+       ro.telephony.ril.v3=icccardstatus,datacall,signalstrength,facilitylock \
+       mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
+       ro.vold.switchablepair=/storage/sdcard0,/storage/sdcard1 \
+	   rild.libpath=/system/lib/libsamsung-ril.so \
+	   ro.sf.lcd_density=240
+
+# enable Google-specific location features,
+# like NetworkLocationProvider and LocationCollector
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.com.google.locationfeatures=1 \
+        ro.com.google.networklocation=1
+
+# Extended JNI checks
+# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
+# before they have a chance to cause problems.
+# Default=true for development builds, set by android buildsystem.
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
+
+# Override /proc/sys/vm/dirty_ratio on UMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vold.umsdirtyratio=20
+
+# We have sacrificed /cache for a larger /system, so it's not large enough for dalvik cache
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-data-only=1
+
+# Set default USB interface and default to internal SD as /sdcard
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mass_storage
+
+include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
+
+# See comment at the top of this file. This is where the other
+# half of the device-specific product definition file takes care
+# of the aspects that require proprietary drivers that aren't
+# commonly available
+$(call inherit-product-if-exists, vendor/samsung/wave/wave-vendor.mk
