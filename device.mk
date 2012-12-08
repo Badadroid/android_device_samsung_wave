3dbac22838302726745858ca9da0cefa8e545e09
Dominik Marszk <dmarszk@gmail.com>
2012-12-08 13:23:30 +0000

    fixup! Cleanup due to merging wave and wave2. Prepared setmodel.sh script for wave2 setup.

--- device.mk
+++ device.mk
@@ -93,8 +93,8 @@ PRODUCT_PACKAGES += \
 # These are the OpenMAX IL configuration files
 PRODUCT_COPY_FILES += \
 	hardware/samsung/exynos3/s5pc110/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
-	device/samsung/wave/media_profiles.xml:system/etc/media_profiles.xml \
-	device/samsung/wave/media_codecs.xml:system/etc/media_codecs.xml
+	device/samsung/wave/prebuilt/media_profiles.xml:system/etc/media_profiles.xml \
+	device/samsung/wave/prebuilt/media_codecs.xml:system/etc/media_codecs.xml
 
 # These are the OpenMAX IL modules
 PRODUCT_PACKAGES += \
