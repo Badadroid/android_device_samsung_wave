796a5d5ced62c1dfe3f0dd844df754e41dddbabf
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-08-03 21:59:32 +0700

    wave: set ro.bq.gpu_to_cpu_unsupported
    This tells SurfaceFlinger to always use the CPU
    pipeline for taking screenshot. We don't actually need
    this since our surface type isn't supported by Bitmap
    and will defaults to CPU anyway, but setting it doesn't
    hurt.
    
    Change-Id: I35c15dff969a89990c0f4ef6512cb79ab3c71362
    Signed-off-by: Pawit Pornkitprasan <p.pawit@gmail.com>
    Signed-off-by: Volk204 <volk204@mail.ru>

--- device.mk
+++ device.mk
@@ -168,8 +168,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
        wifi.interface=wlan0 \
        mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
        ro.vold.switchablepair=/storage/sdcard0,/storage/sdcard1 \
-	   rild.libpath=/system/lib/libmocha-ril.so \
-	   ro.sf.lcd_density=240
+       rild.libpath=/system/lib/libmocha-ril.so \
+       ro.sf.lcd_density=240 \
+       ro.bq.gpu_to_cpu_unsupported=1 \
 
 # SGX540 is slower with the scissor optimization enabled
 PRODUCT_PROPERTY_OVERRIDES += \
