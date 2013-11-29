de5263c0d4d2fecd432010ef814e2140c8f27c1c
Volk204 <volk204@mail.ru>
2013-11-29 22:17:41 +0400

    [AUDIO] Same audio path for SOUND_AUDIO_PATH_BLUETOOTH and SOUND_AUDIO_PATH_BLUETOOTH_NO_NR Confirmed by logs from bada
    Change-Id: Ic1356f8119e1161c1ba6a01efd569bb3fbde4e8f

--- audio-ril-interface/audio-ril-interface.c
+++ audio-ril-interface/audio-ril-interface.c
@@ -205,12 +205,8 @@ int SetAudioPath(HRilClient data, AudioPath path)
 			audio_path.soundType = SND_TYPE_VOICE;
 			break;
 		case SOUND_AUDIO_PATH_BLUETOOTH:
-			audio_path.inDevice = SND_INPUT_BT_MIC;
-			audio_path.outDevice = SND_OUTPUT_BLUETOOTH;
-			audio_path.soundType = SND_TYPE_VOICE;
-			break;
 		case SOUND_AUDIO_PATH_BLUETOOTH_NO_NR:
-			audio_path.inDevice = SND_INPUT_MAIN_MIC;
+			audio_path.inDevice = SND_INPUT_BT_MIC;
 			audio_path.outDevice = SND_OUTPUT_BLUETOOTH;
 			audio_path.soundType = SND_TYPE_VOICE;
 			break;
