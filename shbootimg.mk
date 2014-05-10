2551d5979721286b3697e3e945fd72436f3d05c4
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-05-10 10:40:03 +0700

    wave: create uncompressed ramdisk
    It is more efficient to let the kernel compress it only once. See
    corresponding kernel commit.
    
    http://review.cyanogenmod.org/63721
    
    Change-Id: I8dcd03a4011367b68c96efb23f56348af12da79b
    
    Conflicts:
    	shbootimg.mk

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,7 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
+# Uncompress ramdisk.img to avoid double compression
+uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
+$(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
+	$(MINIGZIP) -d < $(INSTALLED_RAMDISK_TARGET) > $@
+
 # Add ramdisk dependencies to kernel
-TARGET_KERNEL_BINARIES: $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image
+TARGET_KERNEL_BINARIES: $(recovery_ramdisk) $(uncompressed_ramdisk) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET)
