b4408b3d0e50dc2c85e5d14b3e4a8035ea8294b5
Volk204 <volk204@mail.ru>
2014-06-02 20:48:02 +0200

    wave: update recovery_keys
    See http://review.cyanogenmod.org/62724

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -4,19 +4,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-
-int device_toggle_display(volatile char* key_pressed, int key_code) {
-    int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
-    if (alt && key_code == KEY_L)
-        return 1;
-    // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
-    if (ui_get_showing_back_button()) {
-        return 0;
-        //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
-    }
-    return get_allow_toggle_display() && (key_code == KEY_SLEEP);
-}
-
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
@@ -33,13 +20,13 @@ int device_handle_key(int key_code, int visible) {
 				break;
 
             case KEY_MENU:
+            case KEY_SLEEP:
                 return SELECT_ITEM;
 				break;
 
             case KEY_BACK:
                 return GO_BACK;
                 break;
-            case KEY_SLEEP:
             default:
                 return NO_ACTION;
 				break;
