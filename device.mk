dbec057faa1a8cc4556ab999bb0ad6528ebb45db
Volk204 <volk204@mail.ru>
2014-07-22 22:08:03 +0200

    Fix OTG

--- device.mk
+++ device.mk
@@ -145,6 +145,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 
 # The OpenGL ES API level that is natively supported by this device.
 # This is a 16.16 fixed point number
