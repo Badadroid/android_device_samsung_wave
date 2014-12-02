5ad84f7afd5756b75045f9e36a4aea1959848756
Volk204 <volk204@mail.ru>
2014-12-02 19:36:03 +0100

    wave: libcamera: fix compilation

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -10,6 +10,7 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_C_INCLUDES += hardware/samsung/exynos3/s5pc110/include
 LOCAL_C_INCLUDES += hardware/samsung/exynos3/s5pc110/libs3cjpeg
 LOCAL_C_INCLUDES += frameworks/native/include/media/hardware
+LOCAL_C_INCLUDES += system/media/camera/include
 
 LOCAL_SRC_FILES:= \
 	SecCamera.cpp \
