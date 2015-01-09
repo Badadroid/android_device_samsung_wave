c1e44c2c684e4bffd8e1e5cc109ac0c47e89b454
Volk204 <volk204@mail.ru>
2015-01-09 22:23:46 +0100

    Wave: BoardConfig: Explicitly disable SMP
    Building with SMP support is the default now in LP, so disable
    it for our uniprocessor device.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := false
 TARGET_CPU_VARIANT := cortex-a8
 
 TARGET_NO_BOOTLOADER := true
