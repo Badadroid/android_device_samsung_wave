4e2f48ec54c85f071cfc33db3aeeab9e5b2ed346
Humberto Borba <humberos@gmail.com>
2015-03-21 13:48:27 +0100

    wave: : use BOARD_EGL_SYSTEMUI_PBSIZE_HACK
    Here we adjust the PB size from its default value to 512KB which
    is the minimum acceptable for the systemui process.
    We do this on low-end devices only because it allows us to enable
    h/w acceleration in the systemui process while keeping the
    memory usage down.
    
    Obviously, this is the wrong way to make this
    adjustment, but at the time of this writing this was the safest
    solution.
    
    requires: https://gerrit.omnirom.org/12531
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I1608080a5fd11bd4d76352a1fe0ff8486c374bf6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,8 +140,8 @@ MALLOC_IMPL := dlmalloc
 
 # Hardware rendering
 USE_OPENGL_RENDERER := true
-
 BOARD_EGL_CFG := device/samsung/wave/prebuilt/egl.cfg
+BOARD_EGL_SYSTEMUI_PBSIZE_HACK := true
 
 # TARGET_DISABLE_TRIPLE_BUFFERING can be used to disable triple buffering
 # on per target basis. On crespo it is possible to do so in theory
